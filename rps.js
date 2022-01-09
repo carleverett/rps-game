afeba733001e6833f2caffc6f622906bf249cab1
carleverett <carleverett31@gmail.com>
2022-01-09 11:11:13 -0500

    Edit rps.html and rps.js

--- rps.js
+++ rps.js
@@ -1,18 +1,19 @@
 function computerPlay(){
     let choices = ["rock" , "paper", "scissors"];
 
-    let compChoice = choices[Math.floor(Math.random() * choices.length)];
+    let compChoice = choices[Math.floor(Math.random() * choices.length)];   
     return compChoice;
 }
+
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(){
     let selection = prompt("Please choose either rock, paper, or scissors and type the choice into the input box.");
 
     const playerSelection = selection.slice(0).toLowerCase();
     const computerSelection = computerPlay();
-
-    let playerScore = 0;
-    let computerScore = 0;
 
     if(playerSelection === "rock" && computerSelection === "paper"){
         console.log("You lose! Paper beats rock."); 
@@ -35,4 +36,23 @@ function playRound(){
     else(console.log("No winner. Try again."));
 
     console.log(`Score: Player: ${playerScore}, Computer: ${computerScore}`);
+}
+
+function game(){
+    for(i = 1; i<=5; i++){
+        playRound();
+    }
+
+    if(computerScore > playerScore){
+        alert("Sorry! You have lost this round of five.")
+    }
+    else if(playerScore > computerScore){
+        alert("Congratulations! You have won this round of five.")
+    }
+    else if(playerScore = computerScore){
+        alert("This round of five has ended in a tie.")
+    }
+    
+    computerScore = 0;
+    playerScore = 0;
 }
