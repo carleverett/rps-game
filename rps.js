042bd220580c8c31f337c6b447d81e975ddf9a5e
carleverett <carleverett31@gmail.com>
2022-01-09 10:49:21 -0500

    Edit rps.html and rps.js

--- rps.js
+++ rps.js
@@ -2,12 +2,37 @@ function computerPlay(){
     let choices = ["rock" , "paper", "scissors"];
 
     let compChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(compChoice);
+    return compChoice;
 }
 
-function playerSelection(){
+function playRound(){
     let selection = prompt("Please choose either rock, paper, or scissors and type the choice into the input box.");
 
-    let choice = selection.slice(0).toLowerCase();
-    console.log(choice);
+    const playerSelection = selection.slice(0).toLowerCase();
+    const computerSelection = computerPlay();
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    if(playerSelection === "rock" && computerSelection === "paper"){
+        console.log("You lose! Paper beats rock."); 
+        computerScore++;}
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        console.log("You win! Paper beats scissors."); 
+        playerScore++;}
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        console.log("You win! Paper beats rock.");
+        playerScore++;}
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        console.log("You lose! Scissors beat paper.");
+        computerScore++;}
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;}
+    else if(playerSelection === "scissors" && computerSelection === "paper"){ 
+        console.log("You win! Scissors beat paper.");
+        playerScore++;}
+    else(console.log("No winner. Try again."));
+
+    console.log(`Score: Player: ${playerScore}, Computer: ${computerScore}`);
 }
